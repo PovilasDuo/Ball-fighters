ba25728f5389c27ab4f5eb5066df1bcc324c48e4
PovilasDuo <duobapovilas@gmail.com>
2024-10-23 20:10:08 +0300

    Added freeze effect

--- Assets/Scripts/EffectCollision.cs
+++ Assets/Scripts/EffectCollision.cs
@@ -1,13 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EffectCollision : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject explosionGameObject;
+    [SerializeField]
+    private bool spawnOnCollider = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Console.WriteLine(collision.gameObject);
         if(collision.gameObject.tag == "Player")
         {
+            Vector3 position = transform.position;
+            if (spawnOnCollider) position = collision.transform.position;
+            Instantiate(explosionGameObject, position, Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
